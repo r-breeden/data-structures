9bc98f978568ebae186fd29b187eb4da803c0682
Hack Reactor Student <student@hackreactor.com>
2017-07-06 11:04:21 -0700

    Completion of queue prototypal

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+  };
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
-stackMethods
+stackMethods.push = function(value) {
+  this.storage[this.size() + 1] = value;
+};
+
+stackMethods.pop = function() {
+  var popped = this.storage[this.size()];
+  delete this.storage[this.size()];
+  return popped;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
+
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,49 @@
+//Use Object.create with the object from step 2 to create instances of your class
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+ 
+  someInstance.firstOut = undefined;
+  someInstance.lastIn = undefined;
+  someInstance.storage = {};
+  
+  return someInstance;
 };
 
 var queueMethods = {};
 
+//add string to the back of the queue  
+queueMethods.enqueue = function(value) {
+  //if obj is empty
+  var storageSize = this.size();
+
+  if ( storageSize === 0 ) {
+    //add the string to the obj
+    this.storage[0] = value;
+    this.firstOut = 0;
+    this.lastIn = 0;
+  } else {
+    this.storage[this.lastIn + 1] = value;
+    this.lastIn = storageSize;
+  }
+};
+  //remove and return the string at the front of the queue
+queueMethods.dequeue = function() {
+
+  if ( this.size() === 0 ) {
+    return;
+  //if the object is not empty
+  } else {
+    var removed = this.storage[this.firstOut];
+    delete  this.storage[this.firstOut];
+    this.firstOut++;
+    return removed; 
+  }
+};
+  ///return the number of items in the queue
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
