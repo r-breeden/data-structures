bdbc9a3084d7177071f96274d846eee8edcbd010
Hack Reactor Student <student@hackreactor.com>
2017-07-05 21:36:53 -0700

    Changes to functional-shared-queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,37 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
-  var someInstance = {};
-  var lastIn, firstOut;
-  //copy the instances over
-  extend(someInstance, queueMethods);
+
+  var someInstance = {
+    lastIn: undefined,
+    firstOut: undefined,
+    storage: {}
+  };
   
-};
+  //copy the instances over
+  _.extend(someInstance, queueMethods);
+  
+  return someInstance;
+};//end Queue
 
 var queueMethods = {};
   
-
 //add string to the back of the queue  
 queueMethods.enqueue = function(value) {
   //if obj is empty
-  if ( Object.keys(this).length ) {
+  if ( this.size() === 0 ) {
     //add the string to the obj
-    storage[0] = value;
-    firstOut = 0;
-    lastIn = 0;
+    this.storage[0] = value;
+    this.firstOut = 0;
+    this.lastIn = 0;
   } else {
-    storage[lastIn + 1] = value;
-    lastIn = storageSize;
+    this.storage[this.lastIn + 1] = value;
+    this.lastIn = this.size();
   }
 };
   //remove and return the string at the front of the queue
 queueMethods.dequeue = function() {
-  
+  if ( this.size() === 0 ) {
+    return;
+  //if the object is not empty
+  } else {
+    var str = this.storage[this.firstOut];
+    delete this.storage[this.firstOut];
+    this.firstOut++;
+    return str; 
+  }
 };
   ///return the number of items in the queue
 queueMethods.size = function() {
-  return Object.keys(this).length;
+  return Object.keys(this.storage).length;
 };
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -5,4 +5,4 @@ var Stack = function() {
 
 var stackMethods = {};
 
-
+stackMethods
