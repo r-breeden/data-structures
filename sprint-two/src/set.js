b18a57f49ccf852eb406ccb4fa1a9e8815d9f7c7
Ryan Breeden <ryan@ryanbreeden.com>
2017-07-08 15:50:10 -0700

    Completion Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  size = Object.keys(this._storage).length;
+  this._storage[size] = item;
 };
 
 setPrototype.contains = function(item) {
+  for ( var key in this._storage ) {
+    if ( this._storage[key] === item ) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      delete this._storage[key];
+    }
+  }
 };
 
 /*
